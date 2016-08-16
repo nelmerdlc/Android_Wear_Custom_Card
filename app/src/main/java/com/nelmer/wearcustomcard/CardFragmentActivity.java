673fbeb968cae0f4d47a114e6660e7fb75316e39
nelmer <develevel@gmail.com>
2016-08-16 01:09:10 -0400

    This is a demostration on how to create a custom card to display custom content on Android Wear devices.

--- app/src/main/java/com/nelmer/wearcustomcard/CardFragmentActivity.java
+++ app/src/main/java/com/nelmer/wearcustomcard/CardFragmentActivity.java
@@ -1,7 +1,27 @@
 package com.nelmer.wearcustomcard;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
 /**
  * Created by Ros on 8/16/2016.
  */
-public class CardFragmentActivity {
+public class CardFragmentActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //this is where we obtain an instance of the CardFragment in order to add it to the activity.
+        FragmentManager fragmentManager = getFragmentManager();
+        //and here we also use the FragmentTransaction to handle transactions.
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        //so all the content we specified in the CardFragment we will add it to the container created in XML
+        fragmentTransaction.add(R.id.frame_layout, new CustomCard());
+        //and just like github don't forget to commit just to make sure this changes take effect
+        fragmentTransaction.commit();
+    }
 }
--- app/src/main/java/com/nelmer/wearcustomcard/CustomCard.java
+++ app/src/main/java/com/nelmer/wearcustomcard/CustomCard.java
@@ -1,7 +1,19 @@
 package com.nelmer.wearcustomcard;
 
+import android.os.Bundle;
+import android.support.wearable.view.CardFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Ros on 8/16/2016.
  */
-public class CustomCard {
+public class CustomCard extends CardFragment {
+    //the second ViewGroup argument of this method refers to the Activity you are about to attach this Fragment to.
+    @Override
+    public View onCreateContentView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //here you are inflating the custom layout you created to put inside the Fragment
+        return inflater.inflate(R.layout.custom_layout, container, false);
+    }
 }
